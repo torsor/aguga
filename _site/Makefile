3952307ce87cbcf6da291598f9d364ccf07488f5
dkrashen <daniel.krashen@gmail.com>
2014-09-23 09:13:20 -0400

    added gitignore, fixed links, removed _site files

--- _site/Makefile
+++
@@ -1,5 +0,0 @@
-
-all: css/danny.css
-
-css/danny.css: css/danny.scss
-	sass css/danny.scss > css/danny.css
--- _site/js/danny.js
+++
@@ -1,5 +0,0 @@
-$(document).ready(function(){
-	var active_link = $("div.navigation").attr("active_link");
-	var active_link_selector = "#".concat(active_link);
-	$(active_link_selector).addClass("active");
-});
